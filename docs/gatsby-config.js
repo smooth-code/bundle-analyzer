6d7aa2a7876b13a5c69ac2889ff95e2b0a5b6002
Greg Bergé <greg@smooth-code.com>
2019-10-01 14:02:39 +0200

    docs: replace docz by smooth-doc

---
+++ docs/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'smooth-doc',
+      options: {
+        standalone: true,
+        name: 'Bundle Analyzer',
+        slug: 'bundle-analyzer',
+        author: 'Greg Bergé',
+        description: 'Keep your webpack bundle optimized over time.',
+        siteUrl: 'https://docs.bundle-analyzer.com',
+        github: 'https://github.com/smooth-code/bundle-analyzer',
+        menu: ['Getting Started', 'Advanced', 'Troubleshooting'],
+        nav: [{ title: 'Docs', url: '/docs/' }],
+        theme: {
+          colors: {
+            primary: '#0a80cc',
+          },
+        },
+      },
+    },
+  ],
+}
